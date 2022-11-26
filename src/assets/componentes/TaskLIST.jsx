622add6a29f74c3ff10270a142f231952d1fb17c
miguel salazar <migueljoel80@gmail.com>
2022-11-26 12:03:33 -0500

    projecto-4

--- src/assets/componentes/TaskLIST.jsx
+++ src/assets/componentes/TaskLIST.jsx
@@ -2,13 +2,13 @@
 import React from 'react'
 import Task from './Task';
 
-function  TaskList({task, onComplete, onDeleteItem, onEditItem}) {
+function  TaskList({task, onComplete, onDeleteItem, onEditItem, description}) {
   return (
 
     <div className='TaskList'>
         {
-             task.map((task, index)=> (
-            <Task key={`todo-${index}`} task = {task} onComplete = {onComplete} onDeleteItem = {onDeleteItem} onEditItem = {onEditItem}/>
+             task.map((task, index, description)=> (
+            <Task key={`todo-${index}`} task = {task} onComplete = {onComplete} onDeleteItem = {onDeleteItem} onEditItem = {onEditItem} description = {description}/>
         ))
          }
     </div>
